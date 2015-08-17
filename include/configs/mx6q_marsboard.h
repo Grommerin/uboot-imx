ad27fbe49149d1179098bb94e7dfdf7571b4a324
Grommerin <nikita@divakov-PC.(none)>
2015-08-17 16:07:24 +0300

    Изменил команду загрузки

--- include/configs/mx6q_marsboard.h
+++ include/configs/mx6q_marsboard.h
@@ -120,12 +120,8 @@
                 "autostart=yes\0" \
 		"uboot=u-boot.bin\0" \
 		"kernel=uImage\0" \
-		"bootargs=console=ttymxc1,115200\0" \
-		"bootargs_mmc=setenv bootargs ${bootargs} " \
-			"root=/dev/mmcblk0p1 rootwait rw\0" \
-		"bootcmd_mmc=run bootargs_mmc;mmc dev 1;" \
+		"bootcmd=root=/dev/mmcblk0p1 rootwait rw;mmc dev 1;" \
 			"mmc read ${loadaddr} 0x800 0x1400;bootm\0" \
-		"bootcmd=run bootcmd_mmc\0" \
 		"clearenv=sf probe 0 && sf erase 0xc0000 0x2000 && " \
 			"echo restored environment to factory default\0" \
 		"upgradeu=for disk in 0 1 ; do mmc dev ${disk} ;" \
@@ -137,6 +133,12 @@
 			"done\0" \
 		"bootfile=_BOOT_FILE_PATH_IN_TFTP_\0" \
 		"nfsroot=_ROOTFS_PATH_IN_NFS_\0"
+
+/*
+		"bootargs=console=ttymxc1,115200\0" \
+		"bootargs_mmc=setenv bootargs ${bootargs} " \
+			"root=/dev/mmcblk0p1 rootwait rw\0" \
+*/
 
 #define CONFIG_ARP_TIMEOUT	200UL
 
