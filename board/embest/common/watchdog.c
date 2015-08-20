2aba68d9007f5795ee11a62663411290d4cfabc4
Grommerin <nikita@divakov-PC.(none)>
2015-08-20 12:21:58 +0300

    п

--- board/embest/common/watchdog.c
+++ board/embest/common/watchdog.c
@@ -43,12 +43,17 @@ void hw_watchdog_fin(void)
 	printf("MY INSERT: hw_watchdog_fin()\n");
 	struct watchdog_regs *wdog = (struct watchdog_regs *)WDOG1_BASE_ADDR;	
 	u16 reg_wcr = readw(&wdog->wcr);
+	printf("MY INSERT: hw_watchdog_fin()     wcr = %#08x\n", readw(&wdog->wcr));
 	printf("MY INSERT: hw_watchdog_fin() reg_wcr = %#08x\n", reg_wcr);
 	reg_wcr |= 0x0002;
 	printf("MY INSERT: hw_watchdog_fin() reg_wcr = %#08x\n", reg_wcr);
 	writew(reg_wcr, &wdog->wcr);
+	writew(reg_wcr, &wdog->wcr);
+	printf("MY INSERT: hw_watchdog_fin()     wcr = %#08x\n", readw(&wdog->wcr));
 	reg_wcr &= 0xFFFB;
 	printf("MY INSERT: hw_watchdog_fin() reg_wcr = %#08x\n", reg_wcr);
+	writew(reg_wcr, &wdog->wcr);
+	writew(reg_wcr, &wdog->wcr);
 	writew(0x0000, &wdog->wmcr);
-	printf("MY INSERT: hw_watchdog_fin() reg_wcr = %#08x\n", readw(&wdog->wcr));
+	printf("MY INSERT: hw_watchdog_fin()     wcr = %#08x\n", readw(&wdog->wcr));
 }
